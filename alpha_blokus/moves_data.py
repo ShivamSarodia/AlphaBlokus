7da8dbfef835197e7e0697611561035aec64b28e
Shivam Sarodia <ssarodia@gmail.com>
2025-07-08 06:07:19 +0000

    Decompress moves data on load

--- alpha_blokus/moves_data.py
+++ alpha_blokus/moves_data.py
@@ -5,10 +5,13 @@ _MOVES_DATA_CACHE = None
 def _load_moves_data(cfg):
     print("Loading moves data...")
     moves_data_path = cfg["game"]["moves_data_path"]
-    moves = np.load(moves_data_path)
+    compressed_moves = np.load(moves_data_path)
+    moves = {key: compressed_moves[key].copy() for key in compressed_moves.files}
 
     assert moves["new_occupieds"].shape[0] == cfg["game"]["num_moves"]
     assert moves["new_occupieds"].shape[1] == cfg["game"]["board_size"]
+
+    compressed_moves.close()
     
     print("Moves data loaded.")
 
