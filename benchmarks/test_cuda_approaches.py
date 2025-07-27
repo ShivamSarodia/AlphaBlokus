9fc32f9fbc2abc664bea74187530193164382755
Shivam Sarodia <ssarodia@gmail.com>
2025-07-27 07:49:42 +0000

    Improve test CUDA approaches

--- benchmarks/test_cuda_approaches.py
+++ benchmarks/test_cuda_approaches.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import ray
 import numpy as np
 import time
+import threading
 
 # # # # # # # # # # # # # # # # # #
 # Neural network components
@@ -140,16 +141,34 @@ class InferenceActor:
         self.model.to(device=torch.device(DEVICE), dtype=torch.float16)
         self.model.eval()
 
-    async def evaluate_batch(self, boards):
-        boards_tensor = torch.from_numpy(boards.copy()).to(
-            dtype=torch.float16,
-            device=torch.device(DEVICE)
-        )
-        with torch.inference_mode():
-            values_logits_tensor, policy = self.model(boards_tensor)
-            values = torch.softmax(values_logits_tensor, dim=1)
+        self.kernel_lock = threading.Lock()
 
-        return values.cpu().numpy(), policy.cpu().numpy()
+        self.load_data_stream = torch.cuda.Stream()
+        self.kernel_stream = torch.cuda.Stream()
+        self.unload_data_stream = torch.cuda.Stream()
+
+    def evaluate_batch(self, boards):
+        with torch.cuda.stream(self.load_data_stream):
+            boards_tensor = torch.from_numpy(boards.copy()).to(
+                dtype=torch.float16,
+                device=torch.device(DEVICE),
+            )
+
+        with self.kernel_lock:
+            self.kernel_stream.wait_stream(self.load_data_stream)
+            with torch.cuda.stream(self.kernel_stream):
+                with torch.inference_mode():
+                    values_logits_tensor, policy = self.model(boards_tensor)
+                    values = torch.softmax(values_logits_tensor, dim=1)
+
+        self.unload_data_stream.wait_stream(self.kernel_stream)
+        with torch.cuda.stream(self.unload_data_stream):
+            values = values.cpu()
+            policy = policy.cpu()
+
+        # TODO: Add a RecordStream!?
+
+        return values.numpy(), policy.numpy()
 
 # # # # # # # # # # # # # # #
 # Gameplay Actor
@@ -160,13 +179,14 @@ class GameplayActor:
         self.inference_actor = inference_actor
 
     def run(self):
-        while True:
+        start_time = time.time()
+        while time.time() - start_time < 60:
             boards = np.random.randint(0, 1, size=(128, 4, 20, 20))
             values, policy = ray.get(self.inference_actor.evaluate_batch.remote(boards))
             time.sleep(1e-3)
 
 ray.init(log_to_driver=True)
-inference_actor = InferenceActor.remote()
+inference_actor = InferenceActor.options(max_concurrency=50).remote()
 gameplay_actors = [
     GameplayActor.remote(inference_actor)
     for _ in range(12)
@@ -174,4 +194,5 @@ gameplay_actors = [
 for gameplay_actor in gameplay_actors:
     gameplay_actor.run.remote()
 
-time.sleep(60)
+# Give the actors time to wrap up.
+time.sleep(70)
