f054fec2870cb7eb9a33cbe42701b1b8781cdb22
Shivam Sarodia <ssarodia@gmail.com>
2025-08-12 01:21:17 +0000

    Add Vast AI script

--- misc/scripts/vast_setup.sh
+++ misc/scripts/vast_setup.sh
@@ -1,14 +1,33 @@
-#!/bin/bash
+# Disable tmux
+touch ~/.no_auto_tmux
 
-# REPLACE WITH RSYNC?
+# Create a /workspace directory, if needed.
+mkdir -p /workspace
+cd /workspace
+
+# Clone the repo
+git config --global user.email "ssarodia@gmail.com"
+git config --global user.name "Shivam Sarodia"
 git clone https://github.com/ShivamSarodia/AlphaBlokus.git
+
 cd AlphaBlokus
 
-# Install poetry if not already installed
+# Set up my GitHub Personal Access Token so I can push.
+git remote set-url origin https://$GITHUB_PAT@github.com/ShivamSarodia/AlphaBlokus.git
+
+# Install Python and start a venv.
+sudo apt update
+sudo apt install python3 python3-venv python3-pip -y
+python3 -m venv venv
+source venv/bin/activate
+
+# Install the torch-y packages
+pip install torch tensorrt onnx onnxscript cuda-python[all]
+
+# Install poetry and all remaining pacakges
 if ! command -v poetry &> /dev/null; then
     curl -sSL https://install.python-poetry.org | python3 -
     export PATH="$HOME/.local/bin:$PATH"
 fi
 
-# Install dependencies using poetry
 poetry install
