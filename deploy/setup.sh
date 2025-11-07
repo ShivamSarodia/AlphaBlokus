7876b6afcd48db43ed9f1f328731793f6b42b447
Shivam Sarodia <ssarodia@gmail.com>
2025-11-06 23:14:17 -0800

    Minor fixes

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,12 +5,12 @@ trap 'echo "Error: \"$BASH_COMMAND\" failed at line $LINENO" >&2' ERR
 # This script must be called from the root of the repo,
 # after it's already been cloned.
 
-source deploy/setup_components/install_dependencies.sh"
-source deploy/setup_components/install_tensorrt.sh"
-source deploy/setup_components/install_rust.sh"
-source deploy/setup_components/install_node.sh"
-source deploy/setup_components/setup_repo.sh"
-source deploy/setup_components/install_grafana_alloy.sh"
-source deploy/setup_components/run_rust_tests.sh"
+source deploy/setup_components/install_dependencies.sh
+source deploy/setup_components/install_tensorrt.sh
+source deploy/setup_components/install_rust.sh
+source deploy/setup_components/install_node.sh
+source deploy/setup_components/setup_repo.sh
+source deploy/setup_components/install_grafana_alloy.sh
+source deploy/setup_components/run_rust_tests.sh
 
 echo "🎉 Setup complete!"
