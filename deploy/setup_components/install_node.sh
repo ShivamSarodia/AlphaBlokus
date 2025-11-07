471d8ee150d0c668ded70f8348e1836818c2f830
Shivam Sarodia <ssarodia@gmail.com>
2025-11-06 23:16:53 -0800

    Fix node script

--- deploy/setup_components/install_node.sh
+++ deploy/setup_components/install_node.sh
@@ -5,7 +5,8 @@ trap 'echo "Error: \"$BASH_COMMAND\" failed at line $LINENO" >&2' ERR
 echo "⏳ Starting installation of Node..."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
+export NVM_DIR="/opt/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 25
 
 echo "✅ Installed Node"
