e1eacd48ac91c0d9efcd90b2374c05476ecc412b
Shivam Sarodia <ssarodia@gmail.com>
2025-11-09 00:01:35 -0800

    Update tensorrt compilation condition

--- build.rs
+++ build.rs
@@ -3,12 +3,8 @@ fn main() {
     println!("cargo:rerun-if-changed=src/tensorrt/cpp/tensorrt.h");
     println!("cargo:rustc-check-cfg=cfg(cuda)");
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-    if target_os != "linux" {
-        println!(
-            "Skipping TensorRT build script for unsupported target OS: {}",
-            target_os
-        );
+    if !should_compile_tensorrt() {
+        println!("Skipping TensorRT build: CUDA not available");
         return;
     }
 
@@ -25,4 +21,21 @@ fn main() {
     println!("cargo:rustc-link-lib=nvinfer_plugin");
     println!("cargo:rustc-link-lib=nvonnxparser");
     println!("cargo:rustc-link-lib=cudart");
+}
+
+fn should_compile_tensorrt() -> bool {
+    // If we're not on a supported OS (i.e. Linux), skip TensorRT compilation.
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os != "linux" {
+        return false;
+    }
+
+    // If we're in a CI env, skip TensorRT compilation because my GitHub Actions
+    // runner doesn't have an NVIDIA GPU.
+    if std::env::var("CI").unwrap_or_default() == "true" {
+        return false;
+    }
+
+    // Otherwise, compile TensorRT.
+    true
 }
