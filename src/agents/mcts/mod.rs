0b428bd755dc1f2cb592ed2c69a915699c5a9797
Shivam Sarodia <ssarodia@gmail.com>
2026-07-19 19:11:30 -0700

    Separate MCTS search from native recording

--- src/agents/mcts/mod.rs
+++ src/agents/mcts/mod.rs
@@ -1,4 +1,6 @@
 mod agent;
 mod node;
+mod search;
 
 pub use agent::MCTSAgent;
+pub use search::MCTSSearch;
--- src/agents/mod.rs
+++ src/agents/mod.rs
@@ -5,7 +5,7 @@ mod policy_sampling;
 mod random;
 
 pub use base::Agent;
-pub use mcts::MCTSAgent;
+pub use mcts::{MCTSAgent, MCTSSearch};
 pub use pentobi::PentobiAgent;
 pub use policy_sampling::PolicySamplingAgent;
 pub use random::RandomAgent;
